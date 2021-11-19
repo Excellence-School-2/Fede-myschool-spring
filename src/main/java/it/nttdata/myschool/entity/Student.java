7ab3ce2018abc3d0d6c52cbf4210c1c5ca9e5b4f
Federica Bongermino <federica.bonger@gmail.com>
2021-11-19 17:05:01 +0100

    aggiuta

--- src/main/java/it/nttdata/myschool/entity/Student.java
+++ src/main/java/it/nttdata/myschool/entity/Student.java
@@ -7,12 +7,12 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-/**
- * Student
- */
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
 @Entity
 public class Student {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -20,26 +20,18 @@ public class Student {
     private String name;
     private String surname;
     private Integer age;
-    
-    
-    public Student() {}
-   
-    public Student(String name,String surname,Integer age) {
 
-        this.age=age;
-        this.name=name;
-        this.surname=surname;
+    public Student(){}
 
-
-        
+    public Student(String name, String surname, Integer age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
     }
-    
-
-
-
 
     @ManyToOne
-    @JoinColumn(name = "schoolClass_id")
+    @JoinColumn(name ="schoolClass_id")
+    @JsonIgnore
     private SchoolClass schoolClass;
 
 
@@ -47,41 +39,32 @@ public class Student {
         return name;
     }
 
-
     public void setName(String name) {
         this.name = name;
     }
-
 
     public String getSurname() {
         return surname;
     }
 
-
     public void setSurname(String surname) {
         this.surname = surname;
     }
-
 
     public Integer getAge() {
         return age;
     }
 
-
     public void setAge(Integer age) {
         this.age = age;
     }
-
 
     public SchoolClass getSchoolClass() {
         return schoolClass;
     }
 
-
     public void setSchoolClass(SchoolClass schoolClass) {
         this.schoolClass = schoolClass;
     }
-
-
-
+    
 }
